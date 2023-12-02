badcf9dbd92c59af680a12c88fa2ff592eede447
matthewacarson <github-carsonm@snkmail.com>
2023-12-01 23:31:25 -0800

    12/1

---
+++ plots/race_of_victim.R
@@ -0,0 +1,297 @@
+# setwd("C:/Users/madou/OneDrive - UCLA IT Services/PS-Honors/police-killings-github")
+
+# Load Libraries ####
+# library(tidycensus)
+# library(sf)
+# library(tidyverse)
+
+# Run setup file to bring in data to summarize
+source(file = "police-killings-setup.R")
+
+
+# ################################### #
+# Plots using race of the victim ####
+# ################################### #
+
+## ######################## #
+## Using 100 quantiles/percentiles ####
+## ######################## #
+
+
+summary_tables$bin_table_race <-  
+  fatal_enc$joined |> 
+  filter(
+    !is.na(income_bins_100) &
+      race_imputed %in% c(
+        "African-American/Black", 
+        "European-American/White", 
+        "Hispanic/Latino")) |> 
+  count(Income = income_bins_100, race_imputed) |> 
+  rename(Killings = n) |> 
+  mutate(Killings_Per_Yr = Killings / 6)
+
+summary_tables$bin_table_race$Income <- as.numeric(summary_tables$bin_table_race$Income)
+
+# Plot quintiles of number of race of victim and quintile of census tract ####
+# they were killed in
+summary_tables$quiniles_race_victim <-  
+  fatal_enc$joined |> 
+  filter(
+    !is.na(income_quintiles) &
+      race_imputed %in% c(
+        "African-American/Black", 
+        "European-American/White", 
+        "Hispanic/Latino")) |> 
+  count(Income = income_quintiles, race_imputed) |> 
+  rename(Killings = n) |>
+  mutate(Killings_Per_Yr = Killings / 6) |> 
+  select(-Killings)
+
+### plots$race_100 ####
+summary_tables$quintile_race_proportion <- 
+  left_join(
+    x = fatal_enc$joined |> 
+      filter(
+        !is.na(income_quintiles) &
+          race_imputed %in% c(
+            "African-American/Black", 
+            "European-American/White", 
+            "Hispanic/Latino")) |> 
+      count(Income = income_quintiles, race_imputed) |> 
+      rename(Killings_by_Quintile_and_Race = n),
+    y = fatal_enc$joined |> 
+      filter(
+        !is.na(income_quintiles) &
+          race_imputed %in% c(
+            "African-American/Black", 
+            "European-American/White", 
+            "Hispanic/Latino")) |> 
+      count(race_imputed) |> 
+      rename(Killings_Race_Total = n),
+    by = "race_imputed"
+  ) |> 
+  mutate(
+    Proportion = Killings_by_Quintile_and_Race / Killings_Race_Total
+  )
+
+summary_tables$bin_table_race_proportion <- 
+  left_join(
+    x = fatal_enc$joined |> 
+      filter(
+        !is.na(income_bins_100) &
+          race_imputed %in% c(
+            "African-American/Black", 
+            "European-American/White", 
+            "Hispanic/Latino")) |> 
+      count(Income = income_bins_100, race_imputed) |> 
+      rename(Killings_inc_race = n),
+    y = fatal_enc$joined |> 
+      filter(
+        !is.na(income_bins_100) &
+          race_imputed %in% c(
+            "African-American/Black", 
+            "European-American/White", 
+            "Hispanic/Latino")) |> 
+      count(race_imputed) |> 
+      rename(Killings_race_only = n),
+    by = "race_imputed"
+  ) |> 
+  mutate(
+    Proportion = Killings_inc_race / Killings_race_only
+  )
+
+summary_tables$bin_table_race_proportion$Income <- as.numeric(summary_tables$bin_table_race_proportion$Income)
+
+
+ggplot(
+  summary_tables$bin_table_race_proportion, 
+  aes(x = Income, y = Proportion, color = race_imputed)
+) +
+  geom_point() +  
+  geom_smooth(method = "loess", formula = y ~ x, se = F) + 
+  labs(
+    x = "Median Household Income in Census Tracts\n100 Quantiles", 
+    y = "Proportion of Persons Killed Within Each Racial Group", 
+    title = "Lethal Uses of Force",
+    subtitle = "Distribution within racial groups",
+    color = "Race of Victim"
+  ) + scale_x_continuous(breaks = seq(0, 100 , by = 5)) + 
+  theme_light() +
+  scale_color_brewer(palette = "Dark2") +
+  theme()
+
+ggsave(
+  filename = 'plots/plots_by_race/race_100_proportion.png', 
+  dpi = 'retina', 
+  bg = 'white',
+  width = 10.4,
+  height = 4.81)
+
+
+################################################# #
+# Another plot ####
+################################################# #
+
+
+## Race of the victim only plot ####
+summary_tables$race_freq <- 
+  table(fatal_enc$joined$race_imputed) |> 
+  as.data.frame() |> 
+  rename(Race = Var1) |> 
+  filter(Race %in% c(
+    "African-American/Black",
+    "European-American/White",
+    "Hispanic/Latino")
+  ) |> mutate(
+    Prop = Freq / nrow(fatal_enc$joined),
+    Race =
+      case_when(
+        Race == "African-American/Black" ~ "Black",
+        Race == "European-American/White" ~ "White",
+        Race == "Hispanic/Latino" ~ "Latino"
+      )
+  )
+
+summary_tables$race_freq$Race <- factor(
+  x = summary_tables$race_freq$Race,
+  levels = c(
+    "White",
+    "Black",
+    "Latino"
+  )
+)
+
+## Race only plot ####
+##
+ggplot() +
+  geom_bar(
+    data = summary_tables$race_freq,
+    aes(
+      x = Race,
+      y = Prop,
+    ),
+    stat = 'identity',
+    width = 0.5,
+    fill = 'lightblue',
+    color = 'black'
+  ) + 
+  labs(
+    x = "", 
+    y = "Proportion of Total LUOFs",
+    title = "Police Lethal Uses of Force",
+    subtitle = "Race of Victim. Years: [2015-2020]"
+  ) + 
+  coord_flip() +
+  theme_light() +
+  theme(
+    axis.text.x = element_text(size = 12, color = "black"),
+    axis.text.y = element_text(size = 12, angle = 90, hjust = 0.5, color = 'black'),
+    axis.title.x = element_text(size = 13),
+    axis.title.y = element_text(size = 15),
+    plot.title = element_text(size = 18),
+    plot.subtitle = element_text(size = 14),
+    legend.key.size = unit(8, "mm"),
+    legend.title = element_text(size = 15),
+    legend.text = element_text(size = 12),
+    # plot.margin = unit(c(1, 1, 1, 0.5), "cm"),
+    # aspect.ratio = 0.75/2.5
+  ) + scale_y_continuous(breaks = seq(0, 0.5, 0.05))
+
+ggsave(
+       filename = 'plots/plots_by_race/race_proportion_of_total_bar.png', 
+       dpi = 'retina', 
+       bg = 'white',
+       width = 10.4,
+       height = 4.81)
+
+
+#################################################### #
+# Distribution by quintile within race ####
+#################################################### #
+
+
+
+summary_tables$quiniles_race_victim <- 
+  left_join(
+    x = fatal_enc$joined |> 
+      filter(
+        !is.na(income_quintiles) &
+          race_imputed %in% c(
+            "African-American/Black", 
+            "European-American/White", 
+            "Hispanic/Latino")) |>
+      count(Income = income_quintiles,Race = race_imputed) |> 
+      rename(Killings_race_inc = n),
+    y =   fatal_enc$joined |> 
+      filter(
+        !is.na(income_quintiles) &
+          race_imputed %in% c(
+            "African-American/Black", 
+            "European-American/White", 
+            "Hispanic/Latino")) |> 
+      count(Race = race_imputed) |> 
+      rename(Killed_race = n),
+    by = "Race"
+  ) |> mutate(
+    Prop = Killings_race_inc / Killed_race
+  ) |> select(Income, Race, Prop)
+
+summary_tables$quiniles_race_victim <- 
+  summary_tables$quiniles_race_victim |> 
+  add_row(
+    summary_tables$summary_1 |> 
+      select(Income, Race = Majority,Killings_quintile = Killings) |> 
+      mutate(Total_Killed = sum(Killings_quintile)) |> 
+      mutate(Prop = Killings_quintile / Total_Killed) |> 
+      select(Income, Race, Prop)
+  )
+# summary_tables$race_and_income_summary
+
+### Reordering Race factors ####
+###
+summary_tables$quiniles_race_victim$Race <- 
+  factor(
+    summary_tables$quiniles_race_victim$Race, 
+    levels = c("All", "African-American/Black",
+               "European-American/White", "Hispanic/Latino"))
+
+ggplot(
+  data = summary_tables$quiniles_race_victim,
+  aes(x = Race, y = Prop, fill = Income)) +
+  geom_bar(
+    stat = "identity", 
+    position = position_dodge(width = 0.9), 
+    color = 'black', 
+    width = 0.8,
+    linewidth = 0.5) +
+  labs(title = "Police Lethal Uses of Force",
+       subtitle = "Years: [2015-2020]",
+       y = "Proportion LUOF Within Each Racial Group",
+       x = "Race of Victim") +
+  theme_classic() + 
+  theme(
+    axis.text.x = element_text(size = 12, color = "black"),
+    axis.text.y = element_text(size = 12, angle = 90, hjust = 0.5, color = 'black'),
+    axis.title.x = element_text(size = 15),
+    axis.title.y = element_text(size = 15),
+    plot.title = element_text(size = 18),
+    plot.subtitle = element_text(size = 14),
+    legend.key.size = unit(8, "mm"),
+    legend.title = element_text(size = 15),
+    legend.text = element_text(size = 12))
+
+ggsave(plot = plots$inc_and_race_victim,
+       filename = 'plots/inc_and_race_victim.png', 
+       dpi = 'retina', 
+       bg = 'white',
+       width = 10.4,
+       height = 4.81)
+
+summary_tables$quiniles_race_victim[1:20,] |> 
+  mutate(Prop = round(Prop,digits = 3) * 100) |> 
+  pivot_wider(names_from = Race, values_from = Prop) |> 
+  mutate(
+    Income =
+      c("1st Quintile", "2nd Quintile", "3rd Quintile", 
+        "4th Quintile", "5th Quintile")
+  ) |> write_csv(file = "quiniles_race_victim.csv")
