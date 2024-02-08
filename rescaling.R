eeb9a8f284ccb9c72a47c29a11671c131f0ee54f
matthewacarson <github-carsonm@snkmail.com>
2024-02-07 16:35:35 -0800

    renamed variables calculated counterfactual rate

--- rescaling.R
+++ rescaling.R
@@ -16,7 +16,7 @@ source(file = "summary_tables.R")
 
 # These tables use the race of the victim.
 
-summary_tables$race_LUOF_Rescale <-
+summary_tables$race_LUOF_counterfactual <-
   full_join(
     x = summary_tables$race_quint_proportions,
     y = summary_tables$quintile_race_proportion,
@@ -47,58 +47,63 @@ summary_tables$race_LUOF_Rescale <-
     prop_of_LUOF_by_race = Proportion_LUOF
   )
 
-# summary_tables$race_LUOF_Rescale # inspect
+# summary_tables$race_LUOF_counterfactual # inspect
 
 # Calculate the annual rate per 10 million population in tract type
-summary_tables$race_LUOF_Rescale$rate <-
-  summary_tables$race_LUOF_Rescale$LUOF_count_quint_race /
-  summary_tables$race_LUOF_Rescale$pop_quint_race * 10000000 / 6
+summary_tables$race_LUOF_counterfactual$rate <-
+  summary_tables$race_LUOF_counterfactual$LUOF_count_quint_race /
+  summary_tables$race_LUOF_counterfactual$pop_quint_race * 10000000 / 6
 
 # Subset tracts where whites were killed -- quintiles
-summary_tables$race_LUOF_Rescale_White <-
-  summary_tables$race_LUOF_Rescale[summary_tables$race_LUOF_Rescale$race == "White",]
+summary_tables$race_LUOF_counterfactual_White <-
+  summary_tables$race_LUOF_counterfactual[summary_tables$race_LUOF_counterfactual$race == "White",]
 
 # Subset tracts where whites were killed -- quintiles
-summary_tables$race_LUOF_Rescale_POC <-
-  summary_tables$race_LUOF_Rescale[summary_tables$race_LUOF_Rescale$race != "White",]
+summary_tables$race_LUOF_counterfactual_POC <-
+  summary_tables$race_LUOF_counterfactual[summary_tables$race_LUOF_counterfactual$race != "White",]
 
 # Add the proportion of whites in tracts to POC df
-summary_tables$race_LUOF_Rescale_POC$white_prop_in_tract <-
-  summary_tables$race_LUOF_Rescale_White$prop_of_race_in_quintile[match(
-    summary_tables$race_LUOF_Rescale_POC$quintile,
-    summary_tables$race_LUOF_Rescale_White$quintile
+summary_tables$race_LUOF_counterfactual_POC$white_prop_in_tract <-
+  summary_tables$race_LUOF_counterfactual_White$prop_of_race_in_quintile[match(
+    summary_tables$race_LUOF_counterfactual_POC$quintile,
+    summary_tables$race_LUOF_counterfactual_White$quintile
   )]
 
 # Proportion of the race of victim in that tract income quintile
-summary_tables$race_LUOF_Rescale_POC$`prop_of_race_in_quintile*rate` <-
-  summary_tables$race_LUOF_Rescale_POC$prop_of_race_in_quintile * 
-  summary_tables$race_LUOF_Rescale_POC$rate
+summary_tables$race_LUOF_counterfactual_POC$`prop_of_race_in_quintile*rate` <-
+  summary_tables$race_LUOF_counterfactual_POC$prop_of_race_in_quintile * 
+  summary_tables$race_LUOF_counterfactual_POC$rate
 
 # Rescaling as if the Black or Latino proportion living in "x" income tract
 # were the same as the proportion of whites living in "x" income tract.
-summary_tables$race_LUOF_Rescale_POC$rescaled <-
-  summary_tables$race_LUOF_Rescale_POC$rate *
-  summary_tables$race_LUOF_Rescale_POC$white_prop_in_tract
+summary_tables$race_LUOF_counterfactual_POC$counterfactual <-
+  summary_tables$race_LUOF_counterfactual_POC$rate *
+  summary_tables$race_LUOF_counterfactual_POC$white_prop_in_tract
 
 # Rounding the results for display (excluding non-numeric columns)
-summary_tables$race_LUOF_Rescale_rounded <-
-  apply(summary_tables$race_LUOF_Rescale_POC[, 3:12], MARGIN = 2,
+summary_tables$race_LUOF_counterfactual_rounded <-
+  apply(summary_tables$race_LUOF_counterfactual_POC[, 3:12], MARGIN = 2,
         FUN = function(x) round(x = x, digits = 2)) |> as.data.frame()
 
 # Rejoining non-numeric columns
-summary_tables$race_LUOF_Rescale_rounded <-
-  cbind(summary_tables$race_LUOF_Rescale_POC[, 1:2],
-        summary_tables$race_LUOF_Rescale_rounded
+summary_tables$race_LUOF_counterfactual_rounded <-
+  cbind(summary_tables$race_LUOF_counterfactual_POC[, 1:2],
+        summary_tables$race_LUOF_counterfactual_rounded
 )
 
 
-# summary_tables$race_LUOF_Rescale_rounded
+# summary_tables$race_LUOF_counterfactual_rounded
 
 # Calculating overall white rate
 
-mean(summary_tables$race_LUOF_Rescale_White$rate)
+mean(summary_tables$race_LUOF_counterfactual_White$rate)
 
 # ############ #
 # Calculating overall black counterfactual rate 
 
-summary_tables$race_LUOF_Rescale_POC
+summary_tables$race_LUOF_counterfactual_POC |> 
+  aggregate(counterfactual ~ race, FUN = mean)
+
+# 2/7/2024: I'm not sure if this is right because the black & Latino 
+# counterfactual rates are much lower than the white rate.
+
