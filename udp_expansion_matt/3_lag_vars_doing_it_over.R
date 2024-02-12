e7dd000666d6d52861b643d016a79c47e04bec87
matthewacarson <github-carsonm@snkmail.com>
2024-02-12 04:49:10 +0000

    update lag vars script

--- udp_expansion_matt/3_lag_vars_doing_it_over.R
+++ udp_expansion_matt/3_lag_vars_doing_it_over.R
@@ -1,6 +1,31 @@
-library(sp)
-library(raster)
-library(parallel)
+# =====================================================
+# =====================================================
+# DISPLACEMENT TYPOLOGY SET UP
+# =====================================================
+# =====================================================
+
+if (!require(pacman)) install.packages("pacman"); pacman::p_load(googledrive, bit64, fs, data.table, tigris, tidycensus, tidyverse, spdep, raster, sp, parallel, sf)
+# library(raster)
+# library(sp)
+# library(parallel)
+# library(sf)
+
+### Tract data extraction function: add your state here
+st <- c("AL", "AK", "AZ", "AR", "CA", "CO", "CT", 
+        "DE", "DC", "FL", "GA", "HI", "ID", "IL", 
+        "IN", "IA", "KS", "KY", "LA", "ME", "MD", 
+        "MA", "MI", "MN", "MS", "MO", "MT", "NE", 
+        "NV", "NH", "NJ", "NM", "NY", "NC", "ND", 
+        "OH", "OK", "OR", "PA", "RI", "SC", "SD", 
+        "TN", "TX", "UT", "VT", "VA", "WA", "WV", 
+        "WI", "WY")
+
+# Combine tracts for the first two states
+combined_tracts <- raster::union(tracts(st[1], cb = TRUE, class = 'sp'),
+                                 tracts(st[2], cb = TRUE, class = 'sp'))
+
+# Function to combine tracts for a single state
+combine_tracts <- function(i) {tracts(st[i], cb = TRUE, class = 'sp')}
 
 # Create a cluster with 2 cores
 cl <- makeCluster(2)
@@ -12,15 +37,10 @@ clusterEvalQ(cl, {
 })
 
 # Load the necessary package within the parallel workers
-clusterExport(cl, c("tracts", "st"))
-
-# Combine tracts for the first two states
-# combined_tracts <- raster::union(tracts(st[1], cb = TRUE, class = 'sp'),
-                                 # tracts(st[2], cb = TRUE, class = 'sp'))
-
-# Function to combine tracts for a single state
-combine_tracts <- function(i) {tracts(st[i], cb = TRUE, class = 'sp')}
-
+clusterExport(cl, c(
+  "tracts" 
+  ,"st"
+))
 # Export the combine_tracts function to the cluster
 clusterExport(cl, c("combine_tracts"))
 
@@ -32,6 +52,7 @@ combined_tracts <- parLapply(cl, 3:51, function(i) {
 
 # Stop the cluster
 stopCluster(cl)
-
+save.image(file = paste0(data_dir, r_data_folder, 'doing_it_over_1.RData'))
 # Combine the resulting tracts
 combined_tracts <- raster::union(combined_tracts)
+save.image(file = paste0(data_dir, r_data_folder, 'all_tracts_combined_doover.RData'))
